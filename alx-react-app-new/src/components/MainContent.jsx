9f08e83271dc33dec2ca73e7f307b86496f0c09d
OluwatayomiTitus <oluwatayooladejo@gmail.com>
2025-11-16 21:56:41 +0100

    correct user ptag content

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -9,8 +9,7 @@ const MainContent = () => {
     >
       <h2 style={{ color: "#333" }}>Welcome to the Main Content</h2>
       <p style={{ lineHeight: "1.6" }}>
-        Here is where your main app content will go. You can add cities,
-        descriptions, and more.
+       "I love to visit New York, Paris, and Tokyo."
       </p>
     </main>
   );
