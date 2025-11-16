1f70a6fd841a441d471fb7adf7b471ffaf705834
OluwatayomiTitus <oluwatayooladejo@gmail.com>
2025-11-16 22:46:28 +0100

    add correction

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react";
+import UserContext from "./UserContext";
+
 const UserProfile = (props) => {
+  const userData = useContext(UserContext);
+
   return (
     <div>
       <h2>{props.name}</h2>
