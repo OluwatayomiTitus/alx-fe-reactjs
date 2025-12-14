c954c3478fa1857af2797ad546eaf4d79d7fa749
OluwatayomiTitus <oluwatayooladejo@gmail.com>
2025-12-14 21:22:19 +0100

    Create User Registration Form Using Controlled Components

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -4,17 +4,27 @@ export default function RegistrationForm() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  const [errors, setErrors] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!username || !email || !password) {
-      setError("All fields are required");
+    if (!username) {
+      setErrors("Username is required");
       return;
     }
 
-    setError("");
+    if (!email) {
+      setErrors("Email is required");
+      return;
+    }
+
+    if (!password) {
+      setErrors("Password is required");
+      return;
+    }
+
+    setErrors("");
 
     console.log({ username, email, password });
     alert("User registered successfully!");
@@ -24,7 +34,7 @@ export default function RegistrationForm() {
     <form onSubmit={handleSubmit}>
       <h2>Register (Controlled)</h2>
 
-      {error && <p style={{ color: "red" }}>{error}</p>}
+      {errors && <p style={{ color: "red" }}>{errors}</p>}
 
       <input
         type="text"
