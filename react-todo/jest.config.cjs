e9a1fbe855af31d605cc3401324a967ca8ee70da
OluwatayomiTitus <oluwatayooladejo@gmail.com>
2025-12-14 22:35:37 +0100

    Implementing and Testing a Todo List Component in React

--- react-todo/jest.config.cjs
+++ react-todo/jest.config.cjs
@@ -4,4 +4,5 @@ module.exports = {
     "^.+\\.[tj]sx?$": "babel-jest",
   },
   moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
 };
